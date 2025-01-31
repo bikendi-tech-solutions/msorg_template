28d0c80ccf00557a79d37af9105fc19f88ac96bd
bikendi-tech-solutions <bikenditechsolutions@gmail.com>
2025-01-31 23:38:44 +0100

    1.8.7

--- sections/wallet.php
+++ sections/wallet.php
@@ -78,7 +78,7 @@ $user_email = get_userdata($id)->user_email;
 
 $bvn = vp_getuser($id,"myBvn",true);
 $nin = vp_getuser($id,"myNin",true);
-if(vp_getoption('enable_monnify') == "yes"  || vp_getoption('enable_ncwallet') == "yes" || vp_getoption('enable_payvessel') == "yes" || vp_getoption('enable_billstack') == "yes" || vp_getoption('enable_paymentpoint') == "yes"  || vp_getoption('enablesquadco') == "yes" ||  vp_getoption('enablevpay') == "yes"  || vp_getoption('enablekuda') == "yes" && ($bvn != 'false' || $nin != 'false'  ||  vp_getoption('enablevpay') == "yes" ) && (!empty($bvn) || !empty($nin)  ||  vp_getoption('enablevpay') == "yes" )  && (mb_strlen($bvn) > 10  ||  vp_getoption('enablevpay') == "yes" )){
+if(vp_getoption('enable_monnify') == "yes"  || vp_getoption('enable_ncwallet') == "yes" || vp_getoption('enable_payvessel') == "yes" || vp_getoption('enable_billstack') == "yes" || vp_getoption('enable_paymentpoint') == "yes" || vp_getoption('enable_paymentpoint') == "yes"  || vp_getoption('enablesquadco') == "yes" ||  vp_getoption('enablevpay') == "yes"  || vp_getoption('enablekuda') == "yes" && ($bvn != 'false' || $nin != 'false'  ||  vp_getoption('enablevpay') == "yes" ) && (!empty($bvn) || !empty($nin)  ||  vp_getoption('enablevpay') == "yes" )  && (mb_strlen($bvn) > 10  ||  vp_getoption('enablevpay') == "yes" )){
 
   
   if(vp_getoption("charge_method") == "fixed"){
@@ -392,29 +392,58 @@ background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/s
 
 if(vp_getoption('enable_billstack') == "yes"  && vp_getoption("vtupress_custom_billstack") == "yes"){
                     
-    $billstack = "9psb";
-    $billstack_accountname = vp_getuser($id,"billstack_accountname");
-    $billstack_accountnumber = vp_getuser($id,"billstack_accountnumber");
+  $billstack = "9psb";
+  $billstack_accountname = vp_getuser($id,"billstack_accountname");
+  $billstack_accountnumber = vp_getuser($id,"billstack_accountnumber");
 
-    if(vp_getoption("billstack_charge_method") == "fixed"){
-        $billstack_chargef =  "₦".floatval(vp_getoption("billstack_charge_back"));
-      }
-      else{
-        $billstack_chargef =  floatval(vp_getoption("billstack_charge_back"))."%";
-      }
-  ?>
-    
+  if(vp_getoption("billstack_charge_method") == "fixed"){
+      $billstack_chargef =  "₦".floatval(vp_getoption("billstack_charge_back"));
+    }
+    else{
+      $billstack_chargef =  floatval(vp_getoption("billstack_charge_back"))."%";
+    }
+?>
+  
 
-  <li class="nav-item">
-    <a
-      class="nav-link <?php echo banksbtn();?>"
-      data-bs-toggle="tab"
-      href="#billstack"
-      role="tab"
-      ><span class="hidden-sm-up"></span>
-      <span class="hidden-xs-down"><?php echo $billstack;?></span></a
-    >
-  </li>
+<li class="nav-item">
+  <a
+    class="nav-link <?php echo banksbtn();?>"
+    data-bs-toggle="tab"
+    href="#billstack"
+    role="tab"
+    ><span class="hidden-sm-up"></span>
+    <span class="hidden-xs-down"><?php echo $billstack;?></span></a
+  >
+</li>
+
+<?php } 
+
+
+if(vp_getoption('enable_paymentpoint') == "yes"  && vp_getoption("vtupress_custom_paymentpoint") == "yes"){
+                    
+  $paymentpoint = "Palmpay";
+  $paymentpoint_accountname = vp_getuser($id,"paymentpoint_accountname");
+  $paymentpoint_accountnumber = vp_getuser($id,"paymentpoint_accountnumber");
+
+  if(vp_getoption("paymentpoint_charge_method") == "fixed"){
+      $paymentpoint_chargef =  "₦".floatval(vp_getoption("paymentpoint_charge_back"));
+    }
+    else{
+      $paymentpoint_chargef =  floatval(vp_getoption("paymentpoint_charge_back"))."%";
+    }
+?>
+  
+
+<li class="nav-item">
+  <a
+    class="nav-link <?php echo banksbtn();?>"
+    data-bs-toggle="tab"
+    href="#paymentpoint"
+    role="tab"
+    ><span class="hidden-sm-up"></span>
+    <span class="hidden-xs-down"><?php echo $paymentpoint;?></span></a
+  >
+</li>
 
 <?php } 
                 if(vp_getoption('enablekuda') == "yes"  && vp_getoption("vtupress_custom_kuda") == "yes"){
@@ -746,6 +775,61 @@ if(vp_getoption('enable_payvessel') == "yes"  && vp_getoption("vtupress_custom_p
       </div>
     
     <?php }
+
+
+  
+if(vp_getoption('enable_paymentpoint') == "yes"  && vp_getoption("vtupress_custom_paymentpoint") == "yes"){?>
+
+  <div class="tab-pane <?php echo banksmodal();?>" id="paymentpoint" role="tabpanel">
+      <div class="p-md-20">
+  
+      <!-------------CONTENT----------->
+  <div class="Wrap mb-2 cdebit-card   position-relative">
+  <div class="Base">
+  <div class="Inner-wrap">
+  
+  <div class=" container text-white p-4 roundeds">
+  
+  <div class="row mb-3">
+  <div class="col Logo-name fs-3">
+    Palmpay
+  </div>
+  </div>
+  
+  <div class="row mb-3">
+  <div class="col card-number text-center">
+  <p><?php accountNumber($paymentpoint_accountnumber,"paymentpoint");?></p>
+  </div>
+  </div>
+  
+  <div class="row mb-3">
+  <div class="col Name white  text-center">
+  <p><?php echo $paymentpoint_accountname;?></p>
+  </div>
+  </div>
+  
+  
+  <div class="row">
+  <div class="col fs-5">
+  VISA
+  </div>
+  <div class="col flex justify-content-end  fs-5">
+  <?php echo $paymentpoint_chargef;?> Charge Applied
+  </div>
+  </div>
+  
+  </div>
+  
+  </div>
+  </div>
+  </div>
+  
+  
+      <!------------------>
+      </div>
+    </div>
+  
+  <?php }
 
 
 if(vp_getoption('enable_paymentpoint') == "yes"  && vp_getoption("vtupress_custom_paymentpoint") == "yes"){?>
