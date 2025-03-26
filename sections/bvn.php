2fcf74dc05b483fc546ce159c5af11c962c6a8fe
bikendi-tech-solutions <bikenditechsolutions@gmail.com>
2025-03-26 13:30:48 +0100

    1.9.6 new

--- sections/bvn.php
+++ sections/bvn.php
@@ -193,7 +193,11 @@ if(isset($_GET["vend"]) && $_GET["vend"]=="bvn" && vp_option_array($option_array
                                     }).then((value) => {
                                         //?vend=history-plain&id='.$resultsd->id.'&plain_receipt
                                         //location.reload();
-                                        location.href = "?vend=history-plain&id=last&plain_receipt";
+                                        var vtype = jQuery("#vtype").val();
+
+                                        if(vtype == "nin"){
+                                            location.href = "?vend=history-plain&id=last&plain_receipt";
+                                        }
                                     });
                                 }else{
                                     swal({
